a06d592c7e0539aee0f9fb51ca5d5c71b164149e
Oshanz <spiritualmirror@gmail.com>
2014-04-01 16:05:01 +0530

    json sender start

--- sender.php
+++ sender.php
@@ -5,4 +5,11 @@
  */
 $args='{aS:SD,a:1}';
 $shell_exec=shell_exec("java -jar '/home/trisquel/NetBeansProjects/online_print/dist/online_print.jar' ".$args);
-print_r($shell_exec);
+if($shell_exec==1) {
+$file='/var/wwww/print.pdf';
+header("X-Sendfile: $file");
+header("Content-type: application/octet-stream");
+header('Content-Disposition: attachment; filename="'.basename($file).'"');
+} else {
+echo "app failed";
+}
